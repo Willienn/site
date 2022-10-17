b6cc19ac04e63d6f0b46a9f916bb394e0e280469
Willienn <willien_dev@outlook.com>
2022-10-17 14:46:18 -0300

    Feat(Main>Boot)Improve boot

--- pages/boot/index.jsx
+++ pages/boot/index.jsx
@@ -1,41 +1,42 @@
-import {Box} from "@chakra-ui/react";
+import {Box, Flex, Spacer} from "@chakra-ui/react";
 import React, {useEffect, useState} from "react";
 import Typed from "react-typed";
 
 export default function Boot() {
   const [boot, setboot] = useState(false);
   const test = [
-    ["Booting........."],
+    ["Initializing Boot......"],
     ["Checking for errors"],
-    ["Bios ok"],
-    ["SO ok"],
-    ["Kernel ok"],
+    ["Bios ................... OK"],
+    ["Kernel ................... OK"],
+    ["OS ................... OK"],
   ];
-  const [teste, setTeste] = useState("/");
-  const animation = teste;
-  setTimeout(() => {
-    setboot(true);
-  }, 3000);
   useEffect(() => {
-      teste !== "\\" ? setTeste("\\") : setTeste("/");
-  },);
+    setTimeout(() => {
+      setboot(true);
+    }, 2000);
+  }, []);
   return (
     <>
-      <Box>{teste}{teste}{teste}{teste}{teste}{teste}{teste}</Box>
-      {test.map((e, idx) => {
-        return (
-          boot && (
-            <Box color="#11ff11" key={idx}>
-              <Typed
-                startDelay={idx * 600}
-                strings={e}
-                showCursor={false}
-                typeSpeed={0}
-              />
-            </Box>
-          )
-        );
-      })}
+      <Box m='10px'>
+        {test.map((e, idx) => {
+          return (
+            boot && (
+              <Box
+                color="#11ff11"
+                key={idx}
+              >
+                <Typed
+                  startDelay={idx * 1800}
+                  strings={e}
+                  showCursor={false}
+                  typeSpeed={0}
+                />
+              </Box>
+            )
+          );
+        })}
+      </Box>
     </>
   );
 }
