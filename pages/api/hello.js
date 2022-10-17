2bdde8046a66e3348097d5cffbd2ab7ddec54c68
Willienn <willien_dev@outlook.com>
2022-10-17 18:57:28 -0300

    fix?

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,7 @@ const { Client } = require('@notionhq/client');
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-export default async (req, res) => {
+export default async function API(req, res) {
   const response = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID,
   });
