3506a25c3b6d3fc949bfa967a192ef87bf4832e1
Willienn <willien_dev@outlook.com>
2022-11-01 16:16:15 -0300

    Fix(Main)Logo image broken

--- components/nav.jsx
+++ components/nav.jsx
@@ -26,7 +26,7 @@ export default function Nav() {
         >
           <Flex color="white" align="center" fontSize="1.4em" mx="2vw">
             <Link href="/">
-              <Image w="45" h="45" src="/logo.svg"></Image>
+              <Image w="45" h="45" src="/logo.svg" />
             </Link>
             <Spacer />
             {navItens.map((item, idx) => (
