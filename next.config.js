8882e6b835fbc11f5fa45d7b7c98bb1ab828984c
Willien <willien_dev@outlook.com>
2023-07-18 13:57:09 -0300

    Revert "fix build"
    This reverts commit 02776c7f2528e1f6d44270f7a21d312879a7f797.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   reactStrictMode: false,
   swcMinify: true,
   i18n: {
