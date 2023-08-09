da16c93a9313ec8d647025960ee079db0e88660b
Willien <willien_dev@outlook.com>
2023-08-09 11:27:02 -0300

    refactor

--- src/app/api/images/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-
-const images: Array<number> = [];
-
-export async function GET(request: NextRequest) {
-  if (!request.url) return NextResponse.json({}, { status: 200 });
-  images.push(Math.random());
-  return NextResponse.json({ images }, { status: 200 });
-}
