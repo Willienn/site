5ba85079dd38bc11a92a692ae7b952cb1bba821b
Willien <willien_dev@outlook.com>
2024-11-27 10:53:38 -0300

    fix(main): remove async from sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,29 +1,58 @@
 import { posts } from "@/lib/posts/metadata" // Import posts metadata
 import { feeds } from "@/lib/rss/metadata"
+import { MetadataRoute } from "next"
 
-export default async function sitemap() {
+export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://dailycodes.dev"
 
   const staticRoutes = [
-    { url: `${baseUrl}/`, lastModified: new Date().toISOString() },
-    { url: `${baseUrl}/about`, lastModified: new Date().toISOString() },
-    { url: `${baseUrl}/work`, lastModified: new Date().toISOString() },
-  ]
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date().toISOString(),
+      priority: 1,
+      changeFrequency: "yearly",
+    },
+    // {
+    //   url: `${baseUrl}/about`,
+    //   lastModified: new Date().toISOString(),
+    //   changeFrequency: "monthly",
+    //   priority: 1,
+    // },
+    {
+      url: `${baseUrl}/work`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
 
-  const rssRoutes = feeds.map((feed) => ({
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/rss`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ] as MetadataRoute.Sitemap
+
+  const rssRoutes: MetadataRoute.Sitemap = feeds.map((feed) => ({
     url: `${baseUrl}/rss/${feed.slug}`,
     lastModified: new Date().toISOString(),
+    changeFrequency: "weekly",
+    priority: 0.8,
   }))
 
-  const postRoutes = posts.map((post) => ({
+  const postRoutes: MetadataRoute.Sitemap = posts.map((post) => ({
     url: `${baseUrl}/post/${post.slug}`,
     lastModified: new Date(post.updatedAt).toISOString(),
+    changeFrequency: "yearly",
+    priority: 0.7,
   }))
 
   const allRoutes = [...staticRoutes, ...rssRoutes, ...postRoutes]
 
-  return allRoutes.map((route) => ({
-    loc: route.url,
-    lastmod: route.lastModified,
+  return allRoutes.map(({ url, lastModified, priority, changeFrequency }) => ({
+    url,
+    lastModified,
+    priority,
+    changeFrequency,
   }))
 }
